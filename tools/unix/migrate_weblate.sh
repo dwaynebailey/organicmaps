f5625983f0fa5c9245d62145e26748cbd297414c
Dwayne Bailey <dwayne@translate.org.za>
2024-06-27 14:16:28 +0100

    Find missing iPhone localisations

--- tools/unix/migrate_weblate.sh
+++ tools/unix/migrate_weblate.sh
@@ -3,22 +3,29 @@
 # Prepare Android files for Weblate
 langs=$(egrep '=' data/strings/strings.txt | cut -d "=" -f1 | sed "s/[[:space:]]//g" | egrep -v "comment|tags|ref" | cut -d ":" -f 1 | sort -u)
 android_strings_xml=$(find android/app/src/main/res/values* -name "strings.xml" -type f)
+iphone_strings=$(find iphone/Maps/LocalizedStrings/*.lproj -name "Localizable.strings" -type f)
 
 # Resolve any missing languages
 for lang in $langs; do
 	# https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
-	lang=${lang/he/iw}  # Hebrew
-	lang=${lang/id/in}  # Indonesian
-	lang=${lang/zh-Hans/zh}  # Chinese (Simplified)
-	lang=${lang/zh-Hant/zh-TW}  # Chinese (Traditional)
-	lang=${lang/-/-r}   # Region: e.g. en-rGB
 	[[ $lang = "en" ]] && continue  # Skip source language
-	ls android/app/src/main/res/values-$lang/strings.xml >/dev/null 2>&1 || (echo $lang - missing; mkdir -p android/app/src/main/res/values-$lang)
+
+	alang=${lang/he/iw}  # Hebrew
+	alang=${alang/id/in}  # Indonesian
+	alang=${alang/zh-Hans/zh}  # Chinese (Simplified)
+	alang=${alang/zh-Hant/zh-TW}  # Chinese (Traditional)
+	alang=${alang/-/-r}   # Region: e.g. en-rGB
+	ls android/app/src/main/res/values-$alang/strings.xml >/dev/null 2>&1 || (echo $alang - Android missing; mkdir -p android/app/src/main/res/values-$alang)
+
+	ilang=$lang
+	ls iphone/Maps/LocalizedStrings/$ilang.lproj/Localizable.strings >/dev/null 2>&1 || (echo $ilang - iPhone missing; mkdir -p iphone/Maps/LocalizedStrings/$ilang.lproj)
 done
 echo -n "Twine: "
 echo "$langs" | wc -l
 echo -n "Android: "
 echo "$android_strings_xml" | wc -l
+echo -n "iPhone: "
+echo "$iphone_strings" | wc -l
 
 # Perform one last migration from source files
 ./tools/unix/generate_localizations.sh
